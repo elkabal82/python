a13668e32ef2f07387283e7d6663c576ea357121
elkabal82 <etvb66@hotmail.com>
2015-06-12 15:20:13 -0500

    le ponemos una funcion al simple script de hola mundo

--- hmundo/holamundo.py
+++ hmundo/holamundo.py
@@ -2,4 +2,11 @@
 #-*-coding:utf-8-*-
 
 # Solo es el famoso hola mundo
+def saludo():
+
+ 	nombre = raw_input("Hola, Cual es tu nombre: ")
+ 	print "Hola", nombre, "bienvenido a mi primer script"
+ 	raw_input()
+ 
+saludo()
 print "hola mundo"
