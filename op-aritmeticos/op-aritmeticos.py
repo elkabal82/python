489b07381cce183e0eafefb1cbb80c595cf26fef
elkabal82 <etvb66@hotmail.com>
2015-06-20 22:43:48 -0500

    operadores aritmeticos

---
+++ op-aritmeticos/op-aritmeticos.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#-*-encoding:utf-8-*-
+
+# veremos los operadores aritmetico 
+
+suma = 2 + 2
+resta = 2 - 2
+multi = 2 * 2
+div = 2 / 2
+div2 = 3.5 // 1
+exp = 2 ** 2
+residuo = 5 % 2
+
+print suma
+print resta	
+print multi
+print div
+print div2
+print exp
+print residuo
