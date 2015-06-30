7b3813dea7b9a733486f8b7b32e7fbb5bab953c9
elkabal82 <etvb66@hotmail.com>
2015-06-30 16:28:13 -0500

    agregamos unos comentarios

--- modulos/prueba.py
+++ modulos/prueba.py
@@ -13,6 +13,17 @@
 #modulos.saludo()
 #modulos.suma()
 
+'''PEP 8: importación
+La importación de módulos debe realizarse al comienzo del
+documento, en orden alfabético de paquetes y módulos.
+Primero deben importarse los módulos propios de Python.
+Luego, los módulos de terceros y finalmente, los módulos propios
+de la aplicación.
+Entre cada bloque de imports, debe dejarse una línea en blanco. '''
+
 from modulos import saludo, suma
+import this
+
 saludo()
 suma()
+
