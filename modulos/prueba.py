faf9560369117ced8ac1b893dd601784a8e5cc57
elkabal82 <etvb66@hotmail.com>
2015-06-30 16:13:01 -0500

    las formas de importar

--- modulos/prueba.py
+++ modulos/prueba.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 #-*-encoding:utf-8-*-
 
-import modulos
+#(import nombre del modulo)
+#(import paquete.modulo as como la llamaremos despues)
+#(from paquete.modulo import constante)
 
-modulos.saludo()
-modulos.suma()
+#import modulos as m
+#m.saludo()
+#m.suma()
+
+#import modulos
+#modulos.saludo()
+#modulos.suma()
+
+from modulos import saludo, suma
+saludo()
+suma()
