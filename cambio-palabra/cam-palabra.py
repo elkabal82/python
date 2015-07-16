d625ed7a70b54727af4600917f4165542d9999bb
elkabal82 <etvb66@hotmail.com>
2015-07-16 12:23:13 -0500

    en que consite el script

---
+++ cambio-palabra/cam-palabra.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python
+#-*-encoding:utf-8-*-
+
+#Este escrip consiste en que, tienen que ingresar una palabra y el scrip 
+#cambia la palabra segun las siguientes reglas.
+#1.-Si la palabra empieza con vocal, solo se le agrega al final de la palabra
+#"ie"
+#2.-Si temina en consonante, se quita la primera letra y se coloca al final de
+#la palabra + el "ie"
+
